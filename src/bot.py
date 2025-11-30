65fd9515e63c7463853783a902c5ed1e2e6c8ed7
Phillip DiCarlo <sasha-1221@hotmail.com>
2025-11-30 17:56:54 -0500

    Fixed excessive pika logging

--- src/bot.py
+++ src/bot.py
@@ -92,6 +92,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
+logging.getLogger("pika").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
 # -------------------------------------------------------------------
